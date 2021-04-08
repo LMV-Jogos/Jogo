e6c819108d431d74973176b1da40550cb89c39e8
laura-am06 <lauraalaide06@gmail.com>
2021-04-08 20:16:24 +0000

    Finalizar introdução

--- cliente/js/cena01.js
+++ cliente/js/cena01.js
@@ -3,6 +3,7 @@ import { cena1 } from "./cena1.js";
 var cena01 = new Phaser.Scene("Cena 01");
 
 var intro;
+var timedEvent;
 
 cena01.preload = function () {
     this.load.spritesheet("intro", "assets/introdução.png", {
@@ -14,23 +15,16 @@ cena01.preload = function () {
 cena01.create = function () {
     this.anims.create({
         key: "intro",
-        frameRate: 1,
+        frameRate: 0.3,
         frames: this.anims.generateFrameNumbers("intro", { start: 0, end: 11 }),
-        repeat: -1
+        repeat: 0
     });
     intro = this.add.sprite(400, 300, "intro");
     intro.play("intro");
 
-    this.time.addEvent({
-        delay: 3000,
-        callback: () => {
-            intro.once(Phaser.Animations.Events.SPRITE_ANIMATION_COMPLETE, () => {
-            intro.destroy();
-            this.scene.start(cena1);    
-
-        });
-    }
-    });
+    setTimeout(() => {
+        this.scene.start(cena1);
+    }, 40000);
 
 }
    
